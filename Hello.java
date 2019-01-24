38e3a7f1a947e0f1252f11fc06504463f2189b77
PriteshGhogale <46551508+PriteshGhogale@users.noreply.github.com>
2019-01-24 10:06:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*; 
-class Hello { public static void main (String[] args) { System.out.println("Hello World");} }
+class Hello { public static void main (String[] args) { System.out.println("Hello Amar");} }
